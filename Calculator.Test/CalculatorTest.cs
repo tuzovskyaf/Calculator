73e6a8ca16a4d190958552b3d97ab03211c2cc2d
tuzovskyaf <46595929+tuzovskyaf@users.noreply.github.com>
2019-04-20 10:36:05 +0300

    Добавьте файлы проекта.

---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ICalculator
+    {
+        int Add(int num1, int num2);
+        int Mul(int num1, int num2);
+    }
+    public class Calculator : ICalculator
+    {
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        public int Mul(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+    }
+}
